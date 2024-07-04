f580576d43626d0499ccce14c7d2aa42bff18f02
Alexandre Baiocco <abaiocco.autvix@gmail.com>
2024-07-04 17:39:51 -0300

    feat: Adding Floating menu in scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,18 +10,27 @@ public class MenuManager : MonoBehaviour
 
     void Start()
     {
-        
+        // Inicialmente, desativa o menu
+        menu.SetActive(false);
     }
 
     void Update()
     {
-        if(showButton.action.WasPressedThisFrame())
+        if (showButton.action.WasPressedThisFrame())
         {
+            // Alterna o estado ativo do menu
             menu.SetActive(!menu.activeSelf);
 
-            menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-        }
+            // Calcula a nova posição do menu em relação à cabeça
+            Vector3 spawnPosition = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+            menu.transform.position = spawnPosition;
 
-        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+            // Faz o menu olhar para a cabeça
+            Vector3 lookDirection = (head.position - menu.transform.position).normalized;
+            lookDirection.y = 0; // Mantém a rotação no eixo Y
+            menu.transform.rotation = Quaternion.LookRotation(lookDirection);
+
+            menu.transform.forward *= -1;
+        }
     }
 }
