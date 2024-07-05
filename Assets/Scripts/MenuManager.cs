94df9e85e56dbd7e6a608b2617897cef7071f3d3
Alexandre Baiocco <abaiocco.autvix@gmail.com>
2024-07-05 13:40:43 -0300

    feat: Updating menu rotation

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -18,16 +18,21 @@ public class MenuManager : MonoBehaviour
     {
         if (showButton.action.WasPressedThisFrame())
         {
-            // Alterna o estado ativo do menu
             menu.SetActive(!menu.activeSelf);
 
-            // Calcula a nova posição do menu em relação à cabeça
-            Vector3 spawnPosition = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-            menu.transform.position = spawnPosition;
+            if (menu.activeSelf)
+            {
+                // Calcula a nova posição do menu em relação à cabeça
+                Vector3 spawnPosition = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+                menu.transform.position = spawnPosition;
+            }
+        }
 
-            // Faz o menu olhar para a cabeça
+        //Verificação para manter o menu rodando 
+        if (menu.activeSelf)
+        {
             Vector3 lookDirection = (head.position - menu.transform.position).normalized;
-            lookDirection.y = 0; // Mantém a rotação no eixo Y
+            lookDirection.y = 0;
             menu.transform.rotation = Quaternion.LookRotation(lookDirection);
 
             menu.transform.forward *= -1;
