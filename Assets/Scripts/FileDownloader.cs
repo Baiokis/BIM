5c8fedb4925d944a5319dcd97afde764120c4983
Alexandre Baiocco <abaiocco.autvix@gmail.com>
2024-10-24 10:35:13 -0300

    fix: Adjusting comments to English

--- Assets/Scripts/FileDownloader.cs
+++ Assets/Scripts/FileDownloader.cs
@@ -7,10 +7,9 @@ using UnityEngine.SceneManagement;
 
 public class FileDownloader : MonoBehaviour
 {
-    private string fileNameUrl = "http://172.27.90.203:8080/get-latest-file";  // URL para obter o nome do arquivo mais recente
-    private string baseUrl = "http://172.27.90.203:8080/uploads/";  // URL base para o download
-    public string fileName;  // Variável para o nome do arquivo (inicialmente vazia)
-
+    private string fileNameUrl = "http://172.27.90.203:8080/get-latest-file";  
+    private string baseUrl = "http://172.27.90.203:8080/uploads/";  
+    public string fileName;  
 
     [System.Serializable]
     public class FileResponse
@@ -18,14 +17,15 @@ public class FileDownloader : MonoBehaviour
         public string filename;
     }
 
+    // Starts the process to get the filename and download the file
     void Start()
     {
         StartCoroutine(GetFileNameAndDownload());
     }
 
+    // Gets the file name from a remote server and initiates the download process
     IEnumerator GetFileNameAndDownload()
     {
-        // Faz uma requisição GET para obter o nome do arquivo
         UnityWebRequest request = UnityWebRequest.Get(fileNameUrl);
         yield return request.SendWebRequest();
 
@@ -35,16 +35,12 @@ public class FileDownloader : MonoBehaviour
             yield break;
         }
 
-        // Usa JsonUtility para parsear a resposta JSON
         string jsonResponse = request.downloadHandler.text;
         FileResponse fileResponse = JsonUtility.FromJson<FileResponse>(jsonResponse);
 
         if (fileResponse != null && !string.IsNullOrEmpty(fileResponse.filename))
         {
             fileName = fileResponse.filename;
-            Debug.Log("Nome do arquivo obtido: " + fileName);
-
-            // Inicia o download do modelo
             StartCoroutine(DownloadAndLoadModel(fileName));
         }
         else
@@ -53,9 +49,10 @@ public class FileDownloader : MonoBehaviour
         }
     }
 
+    // Downloads the model file and loads it into the scene
     IEnumerator DownloadAndLoadModel(string modelFileName)
     {
-        string url = baseUrl + modelFileName;  // Concatena a URL base com o nome do arquivo
+        string url = baseUrl + modelFileName;
         UnityWebRequest www = UnityWebRequest.Get(url);
         yield return www.SendWebRequest();
 
@@ -67,23 +64,19 @@ public class FileDownloader : MonoBehaviour
 
         string filePath = Path.Combine(Application.dataPath, modelFileName);
         File.WriteAllBytes(filePath, www.downloadHandler.data);
-        Debug.Log("Arquivo salvo em: " + filePath);
 
         AssetDatabase.ImportAsset("Assets/" + modelFileName);
         GameObject model = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/" + modelFileName);
 
         if (model != null)
         {
-            // Verifica se a cena AUTVIX está carregada
             if (SceneManager.GetActiveScene().name != "Autvix")
             {
-                SceneManager.LoadScene("AUTVIX");  // Carrega a cena AUTVIX se ela ainda não estiver ativa
-                yield return null;  // Espera até que a cena seja carregada
+                SceneManager.LoadScene("AUTVIX");  
+                yield return null;  
             }
 
-            // Instancia o modelo na cena AUTVIX
             Instantiate(model, Vector3.zero, Quaternion.identity);
-            Debug.Log("Modelo carregado e instanciado na cena AUTVIX.");
         }
         else
         {
